c342a0461250d4f6c7d197aef52a6fab8c1aa967
Neil Satterfield <neilsatt@gmail.com>
2017-03-05 18:46:44 -0700

    update js file

--- js/pagination.js
+++ js/pagination.js
@@ -1,3 +1,9 @@
+// variables
+let innerLI;
+let anchorTag;
+
+
+
 // Find the total number of students using 'student-item cf' classes
 let totalStudents = document.querySelectorAll('.student-item.cf').length;
 let individualStudent = document.querySelectorAll('.student-item.cf');
@@ -23,35 +29,66 @@ function addPaginationButtons (numOfStudents) {
     let paginationUL = document.createElement('ul');
     document.getElementsByClassName('pagination')[0].appendChild(paginationUL);
     
+  
+    
     // Figure out how many buttons should be added
     let numOfButtons = Math.floor(numOfStudents/10);
     console.log('numBtns: '+numOfButtons); 
 
     // loop through num of buttons to be added, then add the li elements
-     for(i = 0; i<numOfButtons;i++){
-        let innerLI = document.createElement('li');
-        let anchorTag = document.createElement('a');
-        anchorTag.addEventListener('click', doSomething, false);
+     for(i = 0; i<numOfButtons;i++){  
+        innerLI = document.createElement('li');
+        anchorTag = document.createElement('a'); 
+        anchorTag.addEventListener('click', filterStudents, false);
+        anchorTag.setAttribute('id', i+1); 
         anchorTag.setAttribute('href',"#"); 
         anchorTag.innerHTML = i+1;
         innerLI.appendChild(anchorTag);
-        
-
+        if(anchorTag)
+         
         paginationUL.appendChild(innerLI);
+        // make the 1st button active when ititially loading 
+         if(i===0){
+             anchorTag.setAttribute('class',"active");
+         }
          
      }   
-     //anchorTag[0].setAttribute('class',"active");
+     
 }
 
-function doSomething(i) {
-    alert(i);
+// Add the correct students to the page (1-10, 11-20, etc.), and make the button class active
+function filterStudents(e) {
+        var clickedBtn = e.target.id;
+        console.log("Item: "+clickedBtn);
+        console.log(anchorTag);
+        // remove the initial first highlight
+        let firstAnchor = document.getElementById('1')
+        firstAnchor.removeAttribute('class',"active");
+    
+        switch(clickedBtn){
+            case "1":
+                console.log(clickedBtn);
+                this.setAttribute('class',"active");
+                break; 
+            case "2":
+                console.log(clickedBtn);
+                this.setAttribute('class',"active");
+                break; 
+            case "3":
+                console.log(clickedBtn);
+                this.setAttribute('class',"active");
+                break; 
+            default: 
+                console.log('error');
+        }
+            
 }
 
 
 addPaginationButtons(totalStudents);
    
     
-// Add click event to the numbers and add the correct students to the page (1-10, 11-20, etc.), and make the button class active
+// 
 // for()
         // {} anchorTag.setAttribute('class',"active");
 
