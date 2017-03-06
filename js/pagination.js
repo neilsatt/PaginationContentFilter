e1e1dd39c40acd9bbd89f1e961fe52013276655b
Neil Satterfield <neilsatt@gmail.com>
2017-03-05 23:22:48 -0700

    js tests

--- js/pagination.js
+++ js/pagination.js
@@ -24,12 +24,11 @@ function maxStudentsTen() {
 maxStudentsTen();
 
 
-// Add the pagination section to the pagination div, based on how many students there are
+// When page loads, add the pagination section to the pagination div, based on how many students there are
 function addPaginationButtons (numOfStudents) {   
     let paginationUL = document.createElement('ul');
     document.getElementsByClassName('pagination')[0].appendChild(paginationUL);
     
-  
     
     // Figure out how many buttons should be added
     let numOfButtons = Math.floor(numOfStudents/10);
@@ -40,13 +39,14 @@ function addPaginationButtons (numOfStudents) {
         innerLI = document.createElement('li');
         anchorTag = document.createElement('a'); 
         anchorTag.addEventListener('click', filterStudents, false);
+        anchorTag.addEventListener('click', toggleActiveClass, false);
         anchorTag.setAttribute('id', i+1); 
         anchorTag.setAttribute('href',"#"); 
         anchorTag.innerHTML = i+1;
         innerLI.appendChild(anchorTag);
-        if(anchorTag)
-         
         paginationUL.appendChild(innerLI);
+         
+       
         // make the 1st button active when ititially loading 
          if(i===0){
              anchorTag.setAttribute('class',"active");
@@ -56,39 +56,110 @@ function addPaginationButtons (numOfStudents) {
      
 }
 
-// Add the correct students to the page (1-10, 11-20, etc.), and make the button class active
+// Click Event -  Add the correct students to the page (1-10, 11-20, etc.), and make the button class active
+// Use array slices?
 function filterStudents(e) {
-        var clickedBtn = e.target.id;
-        console.log("Item: "+clickedBtn);
-        console.log(anchorTag);
-        // remove the initial first highlight
-        let firstAnchor = document.getElementById('1')
-        firstAnchor.removeAttribute('class',"active");
-    
-        switch(clickedBtn){
-            case "1":
-                console.log(clickedBtn);
-                this.setAttribute('class',"active");
-                break; 
-            case "2":
-                console.log(clickedBtn);
-                this.setAttribute('class',"active");
-                break; 
-            case "3":
-                console.log(clickedBtn);
-                this.setAttribute('class',"active");
-                break; 
-            default: 
-                console.log('error');
-        }
+//    var clickedBtn = e.target.id;
+//    console.log("Clicked Btn: "+clickedBtn);
+//
+//     get reference to all of the buttons
+//    paginationBtns = document.getElementsByClassName(".pagination");
+//    console.log(paginationBtns);
+//    
+//        for (i=0; i<numOfButtons)
+//        if(this != clickedBtn){
+//            this.removeAttribute('class',"active");
+//        }
+//    
+//         remove the initial first highlight
+//        let firstAnchor = document.getElementById('1')
+//        firstAnchor.removeAttribute('class',"active");
+//    
+//        switch(clickedBtn){
+//            case "1":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                removeClassAttribute(clickedBtn);
+//                break; 
+//            case "2":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                removeClassAttribute(clickedBtn);
+//                break; 
+//            case "3":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                removeClassAttribute(clickedBtn);
+//                break; 
+//            case "4":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                removeClassAttribute(clickedBtn);
+//                break; 
+//            case "5":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            case "6":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            case "7":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            case "8":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            case "9":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            case "10":
+//                console.log(clickedBtn);
+//                this.setAttribute('class',"active");
+//                break; 
+//            default: 
+//                console.log('error');
+//        }
             
+}
+
+function toggleActiveClass(e) {
+    let liItems = document.querySelectorAll('div.pagination ul li');
+    console.log("list items: "+liItems);
+    console.log('clicked item: '+e.target.id)
+    
+    if(this.classList.contains('active')){
+          this.classList.remove("active");
+       }
+    else{
+        this.classList.add("active");
+    }
+    
+    
+    
+    
+//    if()
+//    
+//    //Remove class from items NOT clicked
+//         for(i = 1; i<liItems.length; i++)
+//             {
+//                       liItems[i].classList.remove("active");
+//             }
+//          
+}
+
+function removeClassAttribute(e) {
+    console.log('remove attribute: ' +e)
 }
 
 
 addPaginationButtons(totalStudents);
+
+
    
     
-// 
-// for()
-        // {} anchorTag.setAttribute('class',"active");
+
 
